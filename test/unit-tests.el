ff59fba641387a08e622711329c3276f564ed5bb
Josh Bax <joshbax189@gmail.com>
2024-12-02 17:50:10 -0800

    Move tests into own folder and separate files
    This allows separate loading of company and CAPE

--- test/unit-tests.el
+++ test/unit-tests.el
@@ -4,46 +4,8 @@
 (require 'ert)
 (require 'el-mock)
 (require 'ert-async)
-(require 'company) ;; for should-complete
 
-(defun js-comint-test-output-matches (input regex)
-  "Verify that sending INPUT yields output that matches REGEX."
-  (with-new-js-comint-buffer
-    (js-comint-send-string input)
-    (sit-for 1)
-    (let ((output (buffer-substring-no-properties
-                   comint-last-input-end
-                   (car comint-last-prompt))))
-      (should (string-match-p regex output)))))
-
-(defmacro with-new-js-comint-buffer (&rest body)
-  "Run BODY with a fresh js-comint as current buffer and exit after."
-  (declare (indent 0) (debug t))
-  `(progn
-     (when (js-comint-get-process)
-       (kill-process (js-comint-get-process)))
-     (sleep-for 0.2)
-     (kill-matching-buffers (js-comint-get-buffer-name) nil t)
-     (run-js)
-     (unwind-protect
-         (with-current-buffer (js-comint-get-buffer)
-           (font-lock-mode -1)
-           (sit-for 1) ;; prevent race condition on start
-           ,@body)
-       (when (js-comint-get-process)
-         (kill-process (js-comint-get-process)))
-       (sleep-for 0.2)
-       (kill-matching-buffers (js-comint-get-buffer-name) nil t))))
-
-(ert-deftest js-comint-test-multiline-dotchain-line-start ()
-  "Test multiline statement with dots at beginning of lines."
-  (js-comint-test-output-matches
-   "[1, 2, 3]
-  .map((it) => it + 1)
-  .filter((it) => it > 0)
-  .reduce((prev, curr) => prev + curr, 0);"
-   ;; output
-   "^9$"))
+(load-file "./test/common.el")
 
 (ert-deftest js-comint-test-multiline-dotchain-line-start-dos ()
   "Test multiline statement with dots at beginning of lines, with
@@ -490,76 +452,3 @@ Array.valueOf
       ;; clear should be called
       (should (equal (plist-get (car js-comint--completion-callbacks) :type)
                      'clear)))))
-
-;;; Company Integration Tests
-
-;; sanity check: node should interpret ^U
-(ert-deftest js-comint--clear-input-async/test-integration ()
-  "Tests whether clear works in a live comint."
-  (with-new-js-comint-buffer
-    (process-send-string (get-buffer-process (current-buffer)) "5")
-    (js-comint--clear-input-async)
-    (comint-send-input)
-    (let ((output (buffer-substring-no-properties
-                   comint-last-input-end
-                   (car comint-last-prompt))))
-      ;; if it fails, node will see 5^U and fail, or see just 5 and echo it
-      (should (string-empty-p output)))))
-
-(ert-deftest js-comint/test-dumb-term ()
-  "TERM env var should not be dumb."
-  (js-comint-test-output-matches "process.env['TERM']" "emacs"))
-
-(ert-deftest js-comint/test-company-global ()
-  "Tests completion with an empty prompt."
-  (with-new-js-comint-buffer
-    (company-mode)
-    (sit-for 1)
-    (company-manual-begin)
-    ;; register callback to see globals
-    (company-complete-selection)
-    (should (looking-back "AbortController"))))
-
-(ert-deftest js-comint/test-company-unique-result ()
-  "Completing with a unique result.
-E.g. Arr => Array, or conso => console."
-  (with-new-js-comint-buffer
-    (company-mode)
-    (setq company-async-timeout 5)
-    (sit-for 1)
-    (insert "Arra")
-    (company-complete)
-    (should (looking-back "Array"))))
-
-(ert-deftest js-comint/test-company-complete-props ()
-  "Completing props of an object.
-E.g. should complete \"Array.\" to all properties."
-  (with-new-js-comint-buffer
-    (company-mode)
-    (sit-for 1)
-    (insert "Array.")
-    (company-manual-begin)
-    (company-complete-selection)
-    (should (looking-back "Array.__proto__"))))
-
-(ert-deftest js-comint/test-company-complete-long-line ()
-  "Completing part of a line.
-E.g. 'if (true) { console.'"
-  (with-new-js-comint-buffer
-    (company-mode)
-    (sit-for 1)
-    (insert "if (true) { console.")
-    (company-manual-begin)
-    (company-complete-selection)
-    (should (looking-back "console.__proto__"))))
-
-(ert-deftest js-comint/test-company-quick-typing ()
-  "When completion is triggered while one is already running."
-  (with-new-js-comint-buffer
-    (company-mode)
-    (sit-for 1)
-    (insert "scrog.")
-    (company-manual-begin)
-    (insert "foo")
-    (company-manual-begin)
-    (should (looking-back "scrog.foo"))))
